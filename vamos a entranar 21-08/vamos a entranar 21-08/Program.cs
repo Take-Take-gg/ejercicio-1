643741d74971354df1fde8390022aa89afd1f6f0
Alumno <Alumno@PC22>
2019-08-21 12:06:51 -0500

    lacamadafaca

--- vamos a entranar 21-08/vamos a entranar 21-08/Program.cs
+++ vamos a entranar 21-08/vamos a entranar 21-08/Program.cs
@@ -8,7 +8,7 @@ namespace vamos_a_entranar_21_08
         {
             Console.WriteLine("Hello World! x1");
             Console.WriteLine("Hello World! x2");
-            Console.WriteLine("Hello World! x3");
+            Console.WriteLine("hi lakamadafaca");
         }
     }
 }
