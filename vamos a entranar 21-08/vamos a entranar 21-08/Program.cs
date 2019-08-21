8cef0f27928a3d966c146b5143c006d8c9757923
Alumno <Alumno@PC22>
2019-08-21 12:04:11 -0500

    puse mas hola mundo

---
+++ vamos a entranar 21-08/vamos a entranar 21-08/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace vamos_a_entranar_21_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! x1");
+            Console.WriteLine("Hello World! x2");
+            Console.WriteLine("Hello World! x3");
+        }
+    }
+}
