b9548d016f20ec129bce23502ac56eee35aa63fb
Alumno <Alumno@PC22>
2019-08-21 12:18:32 -0500

    este es el ejercicio de mayor y menor

--- vamos a entranar 21-08/vamos a entranar 21-08/Program.cs
+++ vamos a entranar 21-08/vamos a entranar 21-08/Program.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 
-namespace vamos_a_entranar_21_08
+namespace loggin
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! x1");
-            Console.WriteLine("Hello World! x2");
-            Console.WriteLine("hi lakamadafaca");
+            double x, y, z;
+            Console.WriteLine("Imprime los numeros del (x) y (y) y di de cunato en cuanto");
+            Console.WriteLine("empieza el ciclo");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("termina el ciclo");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("¿De cuanto en cunto?");
+            z = Convert.ToDouble(Console.ReadLine());
+
+            if (x < y)
+            {
+                Console.WriteLine("va de menor a mayor");
+                for (double i = x; i < y; i += z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+
+            if (x > y)
+            {
+                Console.WriteLine("va de mayor a menor");
+                for (double i = x; i > y; i -= z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+
         }
     }
 }
